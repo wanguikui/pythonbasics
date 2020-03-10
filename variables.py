c3145ad7e661303b2a1213d6a14fdd4368153169
wanguikui <you@example.comwangui.munyua@gmail.com>
2020-03-10 17:19:24 +0300

    learning Variables strings identifiers

---
+++ variables.py
@@ -0,0 +1,21 @@
+firstName="Wangui"
+lastName="Munyua"
+favNum=4
+student=True
+tall=False
+print(type(firstName))
+print(type(lastName))
+# print(type(favNum))
+print(type(student))
+print(type(tall))
+
+school="TechCamp"
+print(school[3])
+print(school[-5])
+print(school[4:8])
+print(school[4:])
+print(school[-4:])
+
+
+
+
